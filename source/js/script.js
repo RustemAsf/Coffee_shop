bdc6577331b9955014f962e5a8c222ed7db66260
Rustem <arr747@mail.ru>
2022-07-10 11:06:19 +0500

    create menu js

--- source/js/script.js
+++ source/js/script.js
@@ -1,72 +1,58 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-const link = document.querySelector('.js-login');
-const popup = document.querySelector('.modal');
-const close = popup.querySelector('.modal__button--close');
-const form = popup.querySelector('form');
-const login = popup.querySelector('[name=login]');
-const password = popup.querySelector('[name=password]');
-
-let isStorageSupport = true;
-let storage = '';
-
-try {
-  storage = localStorage.getItem('login');
-} catch (err) {
-  isStorageSupport = false;
+navToggle.addEventListener('click', function () {
+if (navMain.classList.contains('main-nav--closed')) {
+  navMain.classList.remove('main-nav--closed');
+navMain.classList.add('main-nav--opened');
+} else {
+  navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--opened');
 }
-
-link.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.add('modal--show');
-
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
 });
 
-close.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.remove('modal--show');
-  popup.classList.remove('modal--error');
+const map = L.map('map')
+  .setView({
+    lat: 59.96831,
+    lng: 30.31750,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/icons/main-pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
 });
 
-form.addEventListener('submit', function (evt) {
-  if (!login.value || !password.value) {
-    evt.preventDefault();
-    popup.classList.remove('modal--error');
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add('modal--error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('login', login.value);
-    }
-  }
-});
+const mainPinMarker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31750,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
+mainPinMarker.addTo(map);
 
-    if (popup.classList.contains('modal--show')) {
-      popup.classList.remove('modal--show');
-      popup.classList.remove('modal--error');
-    }
-  }
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: 59.96831,
+    lng: 30.31750,
+  });
+
+map.setView({
+  lat: 59.96831,
+  lng: 30.31750,
+}, 16);
 });
